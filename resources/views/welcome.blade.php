5c74bf1c71df808e6bc976405ac61cbfbde68d6d
ichami630 <brandonichami@gmail.com>
2026-04-07 11:48:54 +0100

    routes and views

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,5 +9,7 @@
 </head>
 <body>
     <h1>Welcome to my first Laravel project!</h1>
+    <p>Click the button below to view all users</p>
+    <a href="/users" class="btn">View Users</a>
 </body>
 </html>

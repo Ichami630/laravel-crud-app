9fa9a57aaba9d5be204a5e742ffb34513228d38e
ichami630 <brandonichami@gmail.com>
2026-04-07 11:41:57 +0100

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ichami first Laravel App</title>
+</head>
+<body>
+    <h1>Welcome to my first Laravel project!</h1>
+</body>
+</html>

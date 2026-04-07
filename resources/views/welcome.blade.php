c441242a697ba46aa2e88cca79dfa75024a13a4d
ichami630 <brandonichami@gmail.com>
2026-04-07 14:10:53 +0100

    adding css and tailwind css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,7 @@
 <x-layout>
-    <h1>Welcome to my first Laravel project!</h1>
-    <p>Click the button below to view all users</p>
-    <a href="/users" class="btn">View Users</a>
+    <div class="text-center px-8 py-12">
+        <h1>Welcome to my first Laravel project!</h1>
+        <p>Click the button below to view all users</p>
+        <a href="/users" class="btn bg-blue-600 rounded-md px-2 py-1 text-white">View Users</a>
+    </div>
 </x-layout>
